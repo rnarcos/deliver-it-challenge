0f0d6b4ceb00444ecbec917723e35a2a040800dd
Marcos Fernandes <marcos@development.local>
2020-10-03 20:20:02 -0300

    # Global Styles:. Add css reset to app global styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { CollaboratorsListPage } from './pages';
+import { GlobalStyles } from './global/styles/global.styles';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="app">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            component={CollaboratorsListPage}
-          />
-        </Switch>
-      </div>
+      <GlobalStyles />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={CollaboratorsListPage}
+        />
+      </Switch>
     </BrowserRouter>
   );
 }
