197e864a4056a70accd18f50299fd80aad567a2c
Marcos Fernandes <marcos@development.local>
2020-10-03 20:09:46 -0300

    # Navigation: . Add CollaboratorsListPage. . Add navigation structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { CollaboratorsListPage } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={CollaboratorsListPage}
+          />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/CollaboratorsList/CollaboratorsList.page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function CollaboratorsListPage() {
+  return (
+    <h1>hello world</h1>
+  );
+}
