1bfbab9079bc41a950c7ca5fc62fb1fed338859e
Marcos Fernandes <marcos@development.local>
2020-10-04 20:48:51 -0300

    # useFetch: . Add mutate export to useFetch.

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,11 +2,11 @@ import useSWR from 'swr';
 import api from '../global/api';
 
 export function useFetch(resourceURL, options) {
-  const { data, error } = useSWR(resourceURL, async (requestResourceURL) => {
+  const { data, error, mutate } = useSWR(resourceURL, async (requestResourceURL) => {
     const response = await api.get(requestResourceURL, options);
 
     return response.data;
   });
 
-  return { data, error };
+  return { data, error, mutate };
 }
