ffb9eb9c15e39056543db6a2423610dde4012205
Marcos Fernandes <marcos@development.local>
2020-10-03 23:41:53 -0300

    # CollaboratorOverview section: . Improve styles. . Fix React warning.

--- src/pages/CollaboratorsList/sections/CollaboratorOverview/CollaboratorOverview.section.jsx
+++ src/pages/CollaboratorsList/sections/CollaboratorOverview/CollaboratorOverview.section.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import {
+  Grid,
   Card,
   CardActionArea,
   CardHeader,
   Avatar,
+  makeStyles,
 } from '@material-ui/core';
+import styles from './CollaboratorOverview.styles';
 
 const propTypes = {
   id: PropTypes.string.isRequired,
@@ -15,22 +18,37 @@ const propTypes = {
   role: PropTypes.string.isRequired,
 };
 
+const useStyles = makeStyles(styles);
+
 function CollaboratorOverview({
   id,
   name,
   avatar,
   role,
 }) {
+  const classes = useStyles();
   return (
-    <Card>
-      <Link to={`/collaborators/${id}`} component={CardActionArea}>
-        <CardHeader
-          title={name}
-          subheader={role}
-          avatar={<Avatar src={avatar} />}
-        />
-      </Link>
-    </Card>
+    <Grid
+      item
+      container
+    >
+      <Card className={classes.collaboratorOverviewCard}>
+        <Link
+          to={`/collaborators/${id}`}
+          component={({ navigate, children, href }) => (
+            <CardActionArea href={href}>
+              {children}
+            </CardActionArea>
+          )}
+        >
+          <CardHeader
+            title={name}
+            subheader={role}
+            avatar={<Avatar src={avatar} />}
+          />
+        </Link>
+      </Card>
+    </Grid>
   );
 }
 
---
+++ src/pages/CollaboratorsList/sections/CollaboratorOverview/CollaboratorOverview.styles.js
@@ -0,0 +1,5 @@
+export default () => ({
+  collaboratorOverviewCard: {
+    flex: 1,
+  }
+});
