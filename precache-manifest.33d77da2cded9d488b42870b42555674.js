56b7a3a74c4f652ca39da05c4e74a505b634cb62
Marcos Fernandes <marcos@development.local>
2020-10-04 23:55:14 -0300

    Updates

---
+++ precache-manifest.33d77da2cded9d488b42870b42555674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9b1b9bec636e6dad96bb6f912a2db9",
+    "url": "/deliver-it-challenge/index.html"
+  },
+  {
+    "revision": "1e709274770b053081a4",
+    "url": "/deliver-it-challenge/static/js/2.08a3c810.chunk.js"
+  },
+  {
+    "revision": "2321fb33f83f11ac83cbb97d1f2ba6cd",
+    "url": "/deliver-it-challenge/static/js/2.08a3c810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75570076986cb2e8ab32",
+    "url": "/deliver-it-challenge/static/js/main.320174ab.chunk.js"
+  },
+  {
+    "revision": "6ab5be6e6508a7dfb73d",
+    "url": "/deliver-it-challenge/static/js/runtime-main.f9e006da.js"
+  }
+]);
